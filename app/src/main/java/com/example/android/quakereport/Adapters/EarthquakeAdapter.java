c6290b401c707ab78bdf8bb5945176697702cf02
rykuno <rykuno@gmail.com>
2016-07-27 11:06:06 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/android/quakereport/Adapters/EarthquakeAdapter.java
@@ -0,0 +1,43 @@
+package com.example.android.quakereport.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.android.quakereport.Object.Earthquake;
+import com.example.android.quakereport.R;
+
+import java.util.List;
+
+/**
+ * Created by rykuno on 7/26/16.
+ */
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    public EarthquakeAdapter(Context context, List<Earthquake> objects) {
+        super(context, 0, objects);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item, parent, false);
+        }
+
+        Earthquake currentEarthquake = getItem(position);
+        TextView magnitude = (TextView) listItemView.findViewById(R.id.magnitude);
+        TextView location = (TextView) listItemView.findViewById(R.id.location);
+        TextView date = (TextView) listItemView.findViewById(R.id.date);
+
+        magnitude.setText(currentEarthquake.getMagnitude());
+        location.setText(currentEarthquake.getLocation());
+        date.setText(currentEarthquake.getDate());
+
+        return  listItemView;
+    }
+}
---
+++ app/src/main/java/com/example/android/quakereport/Object/Earthquake.java
@@ -0,0 +1,28 @@
+package com.example.android.quakereport.Object;
+
+/**
+ * Created by rykuno on 7/26/16.
+ */
+public class Earthquake {
+    private String mMagnitude;
+    private String mLocation;
+    private String mDate;
+
+    public Earthquake(String magnitude, String location, String date) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mDate = date;
+    }
+
+    public String getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+}
